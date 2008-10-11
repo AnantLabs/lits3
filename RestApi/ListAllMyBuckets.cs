cf96720655d6ba8e9edf1309730239ac9ce264d3
nfarina <nfarina@05c81eb0-9251-11dd-ba31-9f33ba87bbee>
2008-10-11 18:35:59 +0000

    Moving to a more .NET-style API instead of feeling SOAPy.

--- RestApi/ListAllMyBuckets.cs
+++ RestApi/ListAllMyBuckets.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Xml;
 
-namespace LitS3
+namespace LitS3.RestApi
 {
     public class ListAllMyBucketsResult
     {
         public Identity Owner { get; private set; }
-        public ReadOnlyCollection<Bucket> Buckets { get; private set; }
+        public List<Bucket> Buckets { get; private set; }
 
-        private ListAllMyBucketsResult() { }
+        private ListAllMyBucketsResult()
+        {
+            this.Buckets = new List<Bucket>();
+        }
 
         public static ListAllMyBucketsResult FromXml(XmlReader reader)
         {
-            var buckets = new List<Bucket>();
             var result = new ListAllMyBucketsResult();
-            result.Buckets = new ReadOnlyCollection<Bucket>(buckets);
 
             // See http://docs.amazonwebservices.com/AmazonS3/2006-03-01/RESTServiceGET.html
 
@@ -31,7 +31,7 @@ namespace LitS3
             {
                 if (!reader.IsEmptyElement && reader.Read())
                     while (reader.Name == "Bucket")
-                        buckets.Add(Bucket.FromXml(reader));
+                        result.Buckets.Add(Bucket.FromXml(reader));
             }
             else throw new Exception("Expected <Buckets>.");
 
