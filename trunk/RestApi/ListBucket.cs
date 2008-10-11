3a718b97c1739460418ed6d3f6ce2d327ea1844a
nfarina <nfarina@05c81eb0-9251-11dd-ba31-9f33ba87bbee>
2008-10-11 21:02:45 +0000

    More flexible and elegant API, allows for standard one-liners as well as custom requests and stream-based parsing of response XML.

---
+++ trunk/RestApi/ListBucket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LitS3.RestApi
+{
+    public class ListBucketRequest
+    {
+        public string BucketName { get; set; }
+        public string Prefix { get; set; }
+        public string Marker { get; set; }
+        public string Delimiter { get; set; }
+        public string MaxKeys { get; set; }
+    }
+}
