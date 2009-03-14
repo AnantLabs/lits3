55f9fe0d89613fe1d3490478f29dbfc928c61418
azizatif <azizatif@05c81eb0-9251-11dd-ba31-9f33ba87bbee>
2009-03-14 12:26:35 +0000

    Created a branch to propose removal of ListEntryReader by in-lining its Entries implementation directly into S3.ListAllObjects. It simplifies the implementation, reduces one abstract and simplifies use.

---
+++ branches/ListEntryReader-less/LitS3/S3Headers.cs
@@ -0,0 +1,20 @@
+﻿
+namespace LitS3
+{
+    static class S3Headers
+    {
+        public const string AmazonHeaderPrefix = "x-amz-";
+        public const string AmazonDate = "x-amz-date";
+        public const string CannedAcl = "x-amz-acl";
+        public const string MetadataPrefix = "x-amz-meta-";
+        public const string CopySource = "x-amz-copy-source";
+
+        /// <summary>
+        /// This is set to the number of metadata entries not returned in x-amz-meta headers.
+        /// This can happen if you create metadata using an API like SOAP that supports more
+        /// flexible metadata than the REST API. For example, using SOAP, you can create metadata
+        /// whose values are not legal HTTP headers.
+        /// </summary>
+        public const string MissingMetadata = "x-amz-missing-meta";
+    }
+}
